a0791e000881bb73e9b9d8dbf09bf87c07c0aa8b
Rodrigo Sicarelli <rodrigo.sicarelli@gmail.com>
2022-11-16 20:47:57 +0100

    prepare project for multiplatform

---
+++ settings.gradle.kts
@@ -0,0 +1,29 @@
+enableFeaturePreview("VERSION_CATALOGS")
+
+@Suppress("UnstableApiUsage")
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("deps") {
+            from(files("deps.versions.toml"))
+        }
+    }
+}
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        maven("https://jitpack.io")
+    }
+
+    resolutionStrategy {
+        eachPlugin {
+            if (requested.id.toString() == "com.arkivanov.gradle.setup") {
+                useModule("com.github.arkivanov:gradle-setup-plugin:60ac46054c")
+            }
+        }
+    }
+
+    plugins {
+        id("com.arkivanov.gradle.setup")
+    }
+}
