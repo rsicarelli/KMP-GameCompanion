4dcc93e8cb6e10c5612b36f1507c6c6ac87e01d1
Rodrigo Sicarelli <rodrigo.sicarelli@gmail.com>
2022-12-29 17:18:33 +0100

    get rid of multipane

--- feature/home/src/commonMain/kotlin/app/dreamlightpal/home/HomeScreen.kt
+++ feature/home/src/commonMain/kotlin/app/dreamlightpal/home/HomeScreen.kt
@@ -1,23 +1,16 @@
 package app.dreamlightpal.home
 
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.Stable
-import androidx.compose.runtime.derivedStateOf
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import app.dreamlightpal.detail.DetailComponentFactory
 import app.dreamlightpal.detail.DetailScreen
-import app.dreamlightpal.home.HomeScreenDefaults.DetailsPaneWeight
 import app.dreamlightpal.home.HomeScreenDefaults.FullScreenWeight
-import app.dreamlightpal.home.HomeScreenDefaults.ListPaneWeight
-import app.dreamlightpal.home.HomeScreenDefaults.MultiPaneWidthThreshold
 import app.dreamlightpal.list.ListComponentFactory
 import app.dreamlightpal.list.ListScreen
 import com.arkivanov.decompose.ComponentContext
@@ -25,7 +18,6 @@ import com.arkivanov.decompose.ExperimentalDecomposeApi
 import com.arkivanov.decompose.extensions.compose.jetbrains.stack.Children
 import com.arkivanov.decompose.extensions.compose.jetbrains.stack.animation.fade
 import com.arkivanov.decompose.extensions.compose.jetbrains.stack.animation.stackAnimation
-import com.arkivanov.decompose.extensions.compose.jetbrains.subscribeAsState
 import com.arkivanov.decompose.router.stack.ChildStack
 import com.arkivanov.decompose.value.Value
 import org.kodein.di.DI
@@ -41,39 +33,11 @@ fun HomeScreen(componentContext: ComponentContext, di: DI) = withDI(di) {
         HomeComponentHolder(componentContext, listComponentFactory, detailComponentFactory)
     }
 
-    BoxWithConstraints(modifier = Modifier) {
-        val model by homeComponent.models.subscribeAsState()
-
-        val isMultiPane by remember(model) { derivedStateOf { model.isMultiPane } }
-
-        Row(modifier = Modifier.fillMaxSize()) {
-            ListPane(
-                stack = homeComponent.listStack,
-                modifier = Modifier.weight(if (isMultiPane) ListPaneWeight else FullScreenWeight),
-            )
-
-            if (isMultiPane) {
-                Box(modifier = Modifier.weight(DetailsPaneWeight))
-            }
-        }
-
-        Row(modifier = Modifier.fillMaxSize()) {
-            if (isMultiPane) {
-                Box(modifier = Modifier.weight(ListPaneWeight))
-            }
-
-            DetailsPane(
-                stack = homeComponent.detailStack,
-                modifier = Modifier.weight(if (isMultiPane) DetailsPaneWeight else FullScreenWeight),
-            )
-        }
-
-        val isMultiPaneRequired by remember { derivedStateOf { maxWidth >= MultiPaneWidthThreshold } }
-
-        DisposableEffect(isMultiPaneRequired) {
-            homeComponent.setMultiPane(isMultiPaneRequired)
-            onDispose {}
-        }
+    Row(modifier = Modifier.fillMaxSize()) {
+        ListPane(
+            stack = homeComponent.listStack,
+            modifier = Modifier.weight(FullScreenWeight),
+        )
     }
 }
 
@@ -111,6 +75,6 @@ private object HomeScreenDefaults {
     @Stable
     val MultiPaneWidthThreshold = 800.dp
     const val FullScreenWeight = 1F
-    const val ListPaneWeight = 0.4F
-    const val DetailsPaneWeight = 0.6F
+    const val ListPaneWeight = 0.3F
+    const val DetailsPaneWeight = 0.7F
 }
